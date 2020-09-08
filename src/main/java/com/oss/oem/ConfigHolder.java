420607838971efc73bbfa40dabfff2db51895e7a
mayur-oci <68254337+mayur-oci@users.noreply.github.com>
2020-09-08 11:58:17 -0700

    Update ConfigHolder.java

--- src/main/java/com/oss/oem/ConfigHolder.java
+++ src/main/java/com/oss/oem/ConfigHolder.java
@@ -20,22 +20,21 @@ public class ConfigHolder {
     static public String streamName = "multistream01";
     static public String streamPoolName = "demo-stream-pool";
     static public String streamPoolOcid = "ocid1.streampool.oc1.us-sanjose-1.amaaaaaauwpiejqawbcccfmvdkctu5vbmhwlogzsjss4haz7nuepc4ihk3ea";
-
     static public String streamConsumerGroupName = "oem-oci";
     static public String streamConsumerInstanceName = "oem-oci-instance-100";
     static public CreateCursorDetails.Type streamConsumerCursorType = CreateCursorDetails.Type.AtOffset; //CreateCursorDetails.Type.TrimHorizon;
-    static public Long startOffsetForStreamRead = 882310l;// min value is zero/0
+    static public Long startOffsetForStreamRead = 882310l;// min value is zero/0 ... set it from where you want to start reading the metric messages
     static public Integer streamReadChunkSize = 1000;
     static public Boolean runStreamConsumerTillEternity = true;
     
     
     // Oracle Cloud Monitoring Service Configs
     static public String monitoringProfileName = "MAYUR_ADMIN_PHX";
-    static public String monitoringServiceEndpoint = "https://telemetry-ingestion.us-phoenix-1.oraclecloud.com/";
+    static public String monitoringServiceEndpoint = "https://telemetry-ingestion.us-phoenix-1.oraclecloud.com/"; // endpoint changes as per OCI region
     static public String monitoringCompartmentId = "ocid1.compartment.oc1..aaaaaaaa2z4wup7a4enznwxi3mkk55cperdk3fcotagepjnan5utdb3tvakq";
     static public Region monitoringRegion = Region.US_PHOENIX_1;
 
-
+    // namespace and resource group where converted OEM metrics will reside
     static public String namespace = "oem_to_oci_testrun_0";
     static public String resourceGroup = "oem_installation_testrun_8";
 
