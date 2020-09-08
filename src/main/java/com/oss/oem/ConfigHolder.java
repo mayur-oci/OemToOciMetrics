9a5d961202fb3863f2b516f7279b7b98705f7192
mayur-oci <68254337+mayur-oci@users.noreply.github.com>
2020-09-08 11:53:09 -0700

    Update ConfigHolder.java

--- src/main/java/com/oss/oem/ConfigHolder.java
+++ src/main/java/com/oss/oem/ConfigHolder.java
@@ -6,10 +6,16 @@ import com.oracle.bmc.streaming.model.CreateCursorDetails;
 public class ConfigHolder {
 
 
+    // this is OCI Credentials file needed for authentication and authorization as per the profile you choose
     static public String ociConfigFilePath = "~/.oci/config";
 
 
+    // Oracle Streaming Service Consumer Configs
+    
+    // profile for streaming service client
     static public String streamProfileName = "MAYUR_ADMIN_SJC";
+    
+    // Compartment id of the compartment where Stream to read from is located
     static public String streamCompartmentId = "ocid1.compartment.oc1..aaaaaaaa2z4wup7a4enznwxi3mkk55cperdk3fcotagepjnan5utdb3tvakq";
     static public String streamName = "multistream01";
     static public String streamPoolName = "demo-stream-pool";
@@ -21,7 +27,9 @@ public class ConfigHolder {
     static public Long startOffsetForStreamRead = 882310l;// min value is zero/0
     static public Integer streamReadChunkSize = 1000;
     static public Boolean runStreamConsumerTillEternity = true;
-
+    
+    
+    // Oracle Cloud Monitoring Service Configs
     static public String monitoringProfileName = "MAYUR_ADMIN_PHX";
     static public String monitoringServiceEndpoint = "https://telemetry-ingestion.us-phoenix-1.oraclecloud.com/";
     static public String monitoringCompartmentId = "ocid1.compartment.oc1..aaaaaaaa2z4wup7a4enznwxi3mkk55cperdk3fcotagepjnan5utdb3tvakq";
